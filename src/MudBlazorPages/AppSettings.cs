0c05b366b010fb476562cb70a3322938340d18d2
Jerome Bell <jeromebell@gmail.com>
2024-10-12 11:17:40 -0500

    Initial commit

---
+++ src/MudBlazorPages/AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MudBlazorPages;
+
+public enum ExampleEnum
+{
+    None = 0,
+    FirstValue = 1,
+}
+
+public class AppSettings
+{
+    [JsonPropertyName("stringValue")]
+    public string? StringValue { get; set; }
+
+    [JsonPropertyName("intValue")]
+    public int IntValue { get; set; }
+
+    [JsonPropertyName("enumValue")]
+    public ExampleEnum EnumValue { get; set; }
+
+    /// <summary>
+    ///     This property simply demonstrates that non-existent enum values will not cause an exception during deserialization.
+    ///     See <see cref="Util.UnknownEnumConverter"/>
+    /// </summary>
+    [JsonPropertyName("nonexistentEnumValue")]
+    public ExampleEnum NonexistentEnumValue { get; set; }
+}
